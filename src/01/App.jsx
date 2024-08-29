2feb465edc13066f5408efd476ba7115c29f5236
sanderson <cbsanderson.04@gmail.com>
2024-08-29 14:49:37 -0300

    Initial commit

---
+++ src/01/App.jsx
@@ -0,0 +1,19 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <img
+        src="https://img.myloview.com.br/adesivos/foto-nao-encontrada-icone-vector-simbolo-sinal-400-133715057.jpg"
+        alt=""
+      />
+      <h1>Lorem ipsum dolor um</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,
+        ratione sunt? Fugiat adipisci autem molestiae qui reiciendis iste
+        assumenda corrupti. Minima blanditiis unde impedit! Nam doloribus
+        commodi deserunt hic obcaecati.
+      </p>
+    </div>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://curso.infnet.com.br/c/wp-content/uploads/sites/5/2022/12/Infnet-Logo.png"
+        alt=""
+      />
+      <h1>Teste de Perfomance 2 | Fundamentos de React</h1>
+      <ul>
+        <li>
+          <a href="src/01/index.html">TP2.1</a>
+        </li>
+        <li>
+          <a href="#">TP2.2</a>
+        </li>
+        <li>
+          <a href="#">TP2.3</a>
+        </li>
+        <li>
+          <a href="#">TP2.4</a>
+        </li>
+        <li>
+          <a href="#">TP2.5</a>
+        </li>
+        <li>
+          <a href="#">TP2.6</a>
+        </li>
+        <li>
+          <a href="#">TP2.7</a>
+        </li>
+        <li>
+          <a href="#">TP2.8</a>
+        </li>
+        <li>
+          <a href="#">TP2.9</a>
+        </li>
+        <li>
+          <a href="#">TP2.10</a>
+        </li>
+        <li>
+          <a href="#">TP2.11</a>
+        </li>
+        <li>
+          <a href="#">TP2.12</a>
+        </li>
+        <li>
+          <a href="#">TP2.13</a>
+        </li>
+        <li>
+          <a href="#">TP2.14</a>
+        </li>
+        <li>
+          <a href="#">TP2.15</a>
+        </li>
+        <li>
+          <a href="#">TP2.16</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
